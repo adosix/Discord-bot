a7f417151253ba2c47c27dcb834867fd69c36f71
Andrej <andrejjezik@gmail.com>
2020-10-29 22:49:50 +0100

    sending and recieving messages

--- bot.cpp
+++ bot.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -15,13 +14,24 @@
 #include <netinet/in.h>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <regex>
+#include <list> 
+#include <iterator> 
+
 
 #define REQUIRED_ARGUMENT 1
 #define NO_ARGUMENT 0
+const int NUM_SECONDS = 2;
+
+
+std::regex r_id("\"id\": \"(.*?)\""); 
+std::regex r_name("\"name\": \"(.*?)\"");
+std::regex r_last_msg("\"last_message_id\": \"(.*?)\""); 
+std::regex r_content("\"content\": \"(.*?)\""); 
 
 SSL *ssl;
 int sock;
-
+std::string resp("");
 
 void exit_program(int ret_val, std::string msg)
 {
@@ -103,19 +113,24 @@ Arguments* Arguments::parse_arguments(int argc, char **argv)
     
     return arguments;
 }
-
+/**
+     * Function recieve packet and stores it in global variable resp
+     */
 int RecvPacket()
 {
     int len=100;
-    char buf[1000];
-    do {
+    char buf[1000000];
+   while (len > 20){
         len=SSL_read(ssl, buf, 100);
         buf[len]=0;
-        printf("%s\n",buf);
-    } while (len > 0);
-    if (len < 0) {
+        resp.append(buf);
+    } 
+    //printf("%s \n", resp.c_str());
+
+
+    if (len <= 0) {
         int err = SSL_get_error(ssl, len);
-    if (err == SSL_ERROR_WANT_READ)
+        if (err == SSL_ERROR_WANT_READ)
             return 0;
         if (err == SSL_ERROR_WANT_WRITE)
             return 0;
@@ -158,12 +173,99 @@ void log_ssl()
     }
 }
 
+void get_guilds(std::string token){
+    char content[1024];
+    sprintf(content,
+        "GET /api/users/@me/guilds HTTP/1.1\r\n"
+        "Host: discord.com\r\n"
+        "Connection: keep-alive\r\n"
+        "Authorization: Bot %s\r\n\r\n"
+        ,
+    token.c_str());
+    //printf("%s",content);
+    SendPacket(content);
+    RecvPacket();
+}
+
+void get_newer_msgs(std::string token, std::string last_msg,std::string channel){
+    char content[1024];
+    resp= "";
+    std::cout << channel.c_str() << std::endl;
+    sprintf(content,
+    "GET /api/channels/%s/messages?after=%s HTTP/1.1\r\n"
+    "Host: discord.com\r\n"
+    "Authorization: Bot %s\r\n\r\n"
+    ,
+    channel.c_str(),
+    last_msg.c_str(),
+    token.c_str()); 
+    SendPacket(content);
+    RecvPacket();
+}
+
+void check_new_messages(std::string token,std::string last_msgs[], int cnt, std::list <std::string> chosen_channels){
+    std::smatch match;
+    for(const auto& channel : chosen_channels)
+        {
+            char content[1024];
+            resp= "";
+            std::cout << channel.c_str() << std::endl;
+            sprintf(content,
+            "GET /api/channels/%s HTTP/1.1\r\n"
+            "Host: discord.com\r\n"
+            "Authorization: Bot %s\r\n\r\n"
+            ,
+            channel.c_str(),
+            token.c_str());
+            //printf("%s",content);
+
+            SendPacket(content);
+            RecvPacket();
+
+            std::regex_search(resp, match, r_last_msg) ;
+            if((last_msgs[cnt].compare(match.str(1))) != 0 ){
+                resp= "";
+                get_newer_msgs(token, last_msgs[cnt], channel);
+                std::string temp = resp;
+                std::string id_value;
+                while(std::regex_search(temp, match, r_id)) {
+                    id_value = match.str(1); 
+                    temp = match.suffix().str(); 
+                    std::regex_search(temp, match, r_content) ;
+
+                    std::string msg = "{\"content\": \""+match.str(1) +"\"}";
+                    printf("\r contentus: %s", msg.c_str());
+                    sprintf(content,
+                    "POST /api/channels/%s/messages HTTP/1.1\r\n"
+                    "Host: discord.com\r\n"
+                    "Content-Type: application/json\r\n"
+                    "Connection: keep-alive\r\n"
+                    "Content-Length: %ld\r\n"
+                    "Authorization: Bot %s\r\n\r\n"
+                    "%s"
+                    ,
+                    channel.c_str(),
+                    strlen(msg.c_str()),
+                    token.c_str(),
+                    msg.c_str()
+                    );
+                    SendPacket(content);
+                    temp = "";
+                    
+                }
+                last_msgs[cnt]= id_value;
+            }
+            resp= "";
+            cnt++;
+        }
+}
 int main(int argc, char *argv[])
 {
+
+
     char *ip=(char*)malloc(sizeof(char) *100);
     struct in_addr **addr_list;
     Arguments *arguments = Arguments::parse_arguments(argc, argv);
-    //printf("%s",arguments->token.c_str());
 
     int s;
     s = socket(AF_INET, SOCK_STREAM, 0);
@@ -190,8 +292,10 @@ int main(int argc, char *argv[])
         break;
     }
 
+
+   
     sa.sin_family      = AF_INET;
-    sa.sin_addr.s_addr = inet_addr(ip); // address of google.ru
+    sa.sin_addr.s_addr = inet_addr(ip);
     sa.sin_port        = htons (443); 
     socklen_t socklen = sizeof(sa);
     if (connect(s, (struct sockaddr *)&sa, socklen)) {
@@ -219,19 +323,81 @@ int main(int argc, char *argv[])
         fflush(stdout);
         return -1;
     }
-    sprintf(content,
-        "GET /api/channels/768841171505774613/messages HTTP/1.1\r\n"
+
+    //get guilds of the bot
+    get_guilds(arguments->token);
+
+    int n_channels = 0;
+    std::smatch match;
+    std::string temp = resp;
+    while(std::regex_search(temp, match, r_id)) {
+        std::string id_value = match.str(1); 
+        temp = match.suffix().str(); 
+        n_channels++;
+    }
+    std::string guild_ids[n_channels];
+    while(std::regex_search(resp, match, r_id)) {
+        n_channels--;
+        guild_ids[n_channels] = match.str(1); 
+        resp = match.suffix().str(); 
+    }
+
+   
+    std::list <std::string> chosen_channels;
+    for(int i=0; i < sizeof(guild_ids)/sizeof(guild_ids[0]); i++){
+        sprintf(content,
+        "GET /api/guilds/%s/channels HTTP/1.1\r\n"
         "Host: discord.com\r\n"
+        "Connection: keep-alive\r\n"
         "Authorization: Bot %s\r\n\r\n"
         ,
-      arguments->token.c_str());
+        guild_ids[i].c_str(),
+        arguments->token.c_str());
 
-    printf("%s",content);
-    if(SendPacket(content) != 0){
-        return -1;
-    };
-    if(RecvPacket() != 0){
-        return -1;
+        SendPacket(content);
+        RecvPacket();
+        std::string temp = resp;
+        while(std::regex_search(temp, match, r_id)) {
+        std::string id_value = match.str(1); 
+        temp = match.suffix().str(); 
+        std::regex_search(temp, match, r_name) ;
+        if(match.str(1)=="isa-bot"){
+            chosen_channels.push_back(id_value);
+
+        }
+    }   
+    }
+
+    std::string last_msgs[chosen_channels.size()];
+    int cnt =0;
+    for(const auto& channel : chosen_channels)
+        {
+            resp= "";
+            std::cout << channel.c_str() << std::endl;
+            sprintf(content,
+            "GET /api/channels/%s HTTP/1.1\r\n"
+            "Host: discord.com\r\n"
+            "Authorization: Bot %s\r\n\r\n"
+            ,
+            channel.c_str(),
+            arguments->token.c_str());
+
+            SendPacket(content);
+            RecvPacket();
+            std::regex_search(resp, match, r_last_msg) ;
+            last_msgs[cnt] = match.str(1);
+            resp= "";
+            cnt++;
+        }
+ 
+    int i;
+    //infinite loop with cooldown 2 secs
+    for(;;)
+    {
+        // delay for "NUM_SECONDS" seconds
+        for(i = 0 ; i < NUM_SECONDS ; i++) { usleep(1000 * 1000); }
+        int cnt =0;
+        check_new_messages(arguments->token, last_msgs, cnt, chosen_channels);
     }
     return 0;
 }
